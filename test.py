3c315823120248b260512ea9d572534de42e56a3
Alina7991 <malinka321123@icloud.com>
2025-02-13 23:07:37 +0200

    Hi

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def print_hi(name):
-    print(f"Hi, {name}")
+    print(f"Hi, {name}")  # Используй запятую перед f-строкой для читаемости
 
-if __name__ == "__main__":
-    print_hi("Alina")
+if __name__ == "__main__":  # Двойное подчеркивание вокруг name и main
+    print_hi("Alina")  # Вызов функции с аргументом
